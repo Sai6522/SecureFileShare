7f5248b026bb69144dd136bc0cb4bb16b536178f
Venkata Sai Prasad Pulaparthi <104031565+Sai6522@users.noreply.github.com>
2025-06-16 23:11:11 +0530

    Update settings.py

--- secure_file_share/settings.py
+++ secure_file_share/settings.py
@@ -6,16 +6,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Database configuration
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.environ.get('SQLITE_CLOUD_URL', BASE_DIR / 'db.sqlite3'),
-        'OPTIONS': {
-            'driver': 'sqlitecloud',
-            'uri': True,
+# Database settings
+if 'SQLITE_CLOUD_URL' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.environ.get('SQLITE_CLOUD_URL'),
+            'OPTIONS': {
+                'driver': 'sqlitecloud',
+                'uri': True,
+            }
         }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,12 +81,7 @@ WSGI_APPLICATION = 'secure_file_share.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
